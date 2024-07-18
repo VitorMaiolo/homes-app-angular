47ab230c60570b8be5c7cc624cc4160bb83117aa
VitorMaiolo <165791703+VitorMaiolo@users.noreply.github.com>
2024-07-18 09:14:33 -0300

    Add files via upload

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,120 @@
+import { Component } from '@angular/core';
+import { Imovel } from '../imovel';
+import { ImovelService } from '../imovel.service';
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    RouterModule
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+   imoveisDB: Imovel[] = []
+   filtrandoApto = false
+   filtrandoCasa = false
+
+   constructor(private imovelService: ImovelService){
+    this.imoveisDB = this.imovelService.buscarTodosImoveis()
+
+
+   }
+
+    removerAcentos(str:String) {
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+
+    }
+
+    favoritar(imovel: Imovel){
+
+      if (imovel.favorito == false) {
+          imovel.favorito = true
+          const audio = new Audio('som.mp3')
+          audio.play()
+      } else {
+        imovel.favorito = false
+      }
+
+    }
+
+    abrirMapa(){
+      const lat = -22.7062463   
+      const lgn = -47.6544656
+      const url = `https://www.google.com/maps?q=${lat},${lgn}`
+      window.open(url, "_blank")
+    }
+
+    mostrarFavoritos() {
+      const listaFiltrada: Imovel[] = []
+      for (let i = 0; i < this.imoveisDB.length; i++) {
+        const imovel = this.imoveisDB[i];
+        
+        if (imovel.favorito == true) {
+          listaFiltrada.push(imovel)
+        }
+      }
+      this.imoveisDB = listaFiltrada
+    }
+
+    mostrarTodos(desmarcarRadio = false){
+      this.imoveisDB = this.imovelService.buscarTodosImoveis()
+
+      if (desmarcarRadio == true) {
+        const inputCasa: any = document.getElementById('radioCasa')
+        const inputApto: any = document.getElementById('radioApto')
+
+        if (inputCasa || undefined && inputApto || undefined) {
+          inputCasa.checked = false
+          inputApto.checked = false
+        }
+      }
+    }
+
+
+    fitrarPorTipo(tipo: string){
+      this.mostrarTodos()
+
+      const listaFiltrada: Imovel[] = []
+
+      for (let i = 0; i < this.imoveisDB.length; i++) {
+        const imovel = this.imoveisDB[i];
+
+        if (imovel.tipo == tipo) {
+          listaFiltrada.push(imovel)
+        }
+      }
+      this.imoveisDB = listaFiltrada
+
+    }
+  
+
+    listarImoveisComFiltro(texto:String) {
+
+    if (texto == "") {
+        this.imoveisDB = this.imovelService.buscarTodosImoveis()
+    } else {
+      const imoveisFiltrados: Imovel[] = []
+        for (let i = 0; i < this.imoveisDB.length; i++) {
+            const imovel = this.imoveisDB[i]
+
+            const textoM = this.removerAcentos(texto.toUpperCase())
+            const cidadeImovelM = this.removerAcentos(imovel.cidade.toUpperCase())
+            const estadoImovelM = this.removerAcentos(imovel.estado.toUpperCase())
+
+            if (cidadeImovelM.search(textoM) == 0 || estadoImovelM.search(textoM) == 0) {
+                
+              imoveisFiltrados.push(imovel)
+            }
+        }
+        this.imoveisDB = imoveisFiltrados
+    }
+}
+
+}
+
+
---
+++ src/app/imovel.service.ts
@@ -0,0 +1,198 @@
+import { Injectable } from '@angular/core';
+import { Imovel } from './imovel';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class ImovelService {
+
+ private imoveisDB = [
+  {
+      "id": 1,
+      "url_foto": "1.jpg",
+      "nome": "Casa 01",
+      "cidade": "Saltinho",
+      "estado": "São Paulo",
+      "favorito": false,
+      "tipo":"casa",
+      "latitude": "-22.8457824",
+      "longitude": "-47.6760854",
+      "adicionais": [
+          {
+              "chave":"Quartos disponíveis",
+              "valor": 6
+          },
+          {
+              "chave":"Piscina",
+              "valor":"sim"
+          }
+      ]
+  },
+
+  {
+      "id": 2,
+      "url_foto": "2.jpg",
+      "nome": "Casa 02",
+      "cidade": "Charqueada",
+      "estado": "São Paulo",
+      "favorito": false,
+      "tipo":"apartamento",
+      "latitude": "-22.5104659",
+      "longitude": "-47.7765656",
+      "adicionais": [
+          {
+              "chave":"Banheiro",
+              "valor": 1
+          }  
+      ]
+
+  },
+
+  {
+      "id": 3,
+      "url_foto": "3.jpg",
+      "nome": "Casa 03",
+      "cidade": "Americana",
+      "estado": "São Paulo",
+      "favorito": false,
+      "tipo":"casa",
+      "latitude": "-22.7274743",
+      "longitude": "-47.3080925",
+      "adicionais": [
+          {
+              "chave":"Quartos disponíveis",
+              "valor": 10
+          },
+          {
+              "chave":"Piscina",
+              "valor":"sim"
+          },
+          {
+              "chave":"Quintal",
+              "valor":"Muito grande"
+          },
+          {
+              "chave":"Árvores",
+              "valor":"Frutiferas"
+          }
+
+      ]
+  },
+
+  {
+      "id": 4,
+      "url_foto": "4.jpg",
+      "nome": "Casa 04",
+      "cidade": "São Pedro",
+      "estado": "São Paulo",
+      "favorito": false,
+      "tipo":"casa",
+      "latitude": "-22.5770379",
+      "longitude": "-47.927497",
+      "adicionais":[]
+  },
+
+  {
+      "id": 5,
+      "url_foto": "5.jpg",
+      "nome": "Casa 05",
+      "cidade": "Nova Odessa",
+      "estado": "São Paulo",
+      "favorito": false,
+      "tipo":"casa",
+      "latitude": "-22.7964492",
+      "longitude": "-47.2957863",
+      "adicionais":[]
+  },
+
+
+  {
+      "id": 6,
+      "url_foto": "6.jpg",
+      "nome": "Casa 06",
+      "cidade": "Piracicaba",
+      "estado": "São Paulo",
+      "favorito": false,
+      "tipo":"casa",
+      "latitude": "-22.7139054",
+      "longitude": "-47.6656525",
+      "adicionais":[]
+  },
+
+  {
+      "id": 7,
+      "url_foto": "7.jpg",
+      "nome": "Casa 07",
+      "cidade": "Jundiaí",
+      "estado": "São Paulo",
+      "favorito": false,
+      "tipo":"casa",
+      "latitude": "-23.0987878",
+      "longitude": "-47.0075033",
+      "adicionais":[]
+  },
+
+  {
+      "id": 8,
+      "url_foto": "8.jpg",
+      "nome": "Casa 08",
+      "cidade": "Sumaré",
+      "estado": "São Paulo",
+      "favorito": false,
+      "tipo":"casa",
+      "latitude": "-22.8304558",
+      "longitude": "-47.2675766",
+      "adicionais":[]
+  },
+  {
+      "id": 9,
+      "url_foto": "9.jpg",
+      "nome": "Casa 09",
+      "cidade": "Rio Claro",
+      "estado": "São Paulo",
+      "favorito": false,
+      "tipo":"apartamento",
+      "latitude": "-22.3848445",
+      "longitude": "-47.5469755",
+      "adicionais":[]
+  },
+
+  {
+      "id": 10,
+      "url_foto": "10.jpg",
+      "nome": "Casa 10",
+      "cidade": "Saltinho",
+      "estado": "São Paulo",
+      "favorito": false,
+      "tipo":"casa",
+      "latitude": "-22.8446977",
+      "longitude": "-47.6797359",
+      "adicionais":[]
+  },
+
+] 
+
+
+
+ buscarTodosImoveis(): Imovel[]{
+  return this.imoveisDB
+}
+
+ buscaImovelPeloId(id:number) : Imovel|undefined{
+  let imovel;
+
+  for(let i=0;i< this.imoveisDB.length;i++){
+      imovel = this.imoveisDB[i];
+      if(imovel.id == id){
+          break
+      }
+  }
+  return imovel
+}
+
+}
+
+
